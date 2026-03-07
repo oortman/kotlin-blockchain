0411049b1640d15b05c67b840e7acabe91297be1
Alec Oortman <aoortman1@proton.me>
2026-03-07 14:55:27 -0500

    Added mining before a block is added to the blockchain

--- app/src/main/kotlin/blockchain/Block.kt
+++ app/src/main/kotlin/blockchain/Block.kt
@@ -5,14 +5,26 @@ data class Block(
         val timestamp: Long,
         val data: String,
         val previousHash: String,
+        val nonce: Int = 0
 ) {
-    val hash = computeHash(index, timestamp, data, previousHash)
+    val hash = computeHash(nonce, index, timestamp, data, previousHash)
 
     companion object {
-        fun computeHash(index: Int, timestamp: Long, data: String, previousHash: String): String {
+        fun computeHash(nonce: Int, index: Int, timestamp: Long, data: String, previousHash: String): String {
             val digest = java.security.MessageDigest.getInstance("SHA-256")
-            val hashByteArray = digest.digest("$index$timestamp$data$previousHash".toByteArray())
+            val hashByteArray = digest.digest("$nonce$index$timestamp$data$previousHash".toByteArray())
             return hashByteArray.joinToString("") { "%02x".format(it) }
         }
+    }
+
+    fun mine(difficulty: Int): Block {
+        var block = this
+        val target = "0".repeat(difficulty)
+        
+        while(!block.hash.startsWith(target)) {
+            block = block.copy(nonce = block.nonce + 1)
+        }
+
+        return block
     }
 }
--- app/src/main/kotlin/blockchain/Blockchain.kt
+++ app/src/main/kotlin/blockchain/Blockchain.kt
@@ -9,11 +9,12 @@ object Blockchain {
 
     fun addBlock(data: String) {
         val previous = chain.last()
-        chain.add(Block(previous.index + 1, System.currentTimeMillis(), data, previous.hash))
+        val block = Block(previous.index + 1, System.currentTimeMillis(), data, previous.hash)
+        chain.add(block.mine(5))
     }
 
     fun isValid(): Boolean {
-        val validHashes = chain.all { it.hash == Block.computeHash(it.index, it.timestamp, it.data, it.previousHash) }
+        val validHashes = chain.all { it.hash == Block.computeHash(it.nonce, it.index, it.timestamp, it.data, it.previousHash) }
         val validLinks = chain.zipWithNext().all { it.second.previousHash == it.first.hash }
 
         return validHashes && validLinks
