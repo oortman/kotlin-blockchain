9c26d5b3953cbefeecb15597fd8a60f2965cad9c
Alec Oortman <aoortman1@proton.me>
2026-03-10 12:14:49 -0400

    Updating configs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,14 @@
-plugins {
-    // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
 pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
+}
+
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "kotlin-blockchain"
